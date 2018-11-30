57ff321253a9826126e9a75be39a0dd9b2f86dcd
Gustavo Freire Santos <gustavofreiresantos@Gustavos-MacBook-Pro.local>
2018-11-29 22:09:07 -0300

    improve this project to have better touch capabilities

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -3,18 +3,45 @@ import '../../stylesheets/styles.scss';
 class HomePage extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			element: null,
+		};
+		this.scaleButton = this.scaleButton.bind(this);
+	}
+	scaleButton(event){
+		const { element } =  this.state;
+		if (element){
+			if (element !== event.target){
+				element.classList.toggle("scaled");
+				this.setState({
+					element: event.target,
+				})
+				event.target.classList.toggle("scaled");
+			}
+			else {
+				element.classList.toggle("scaled");
+				this.setState({
+					element: null,
+				})
+			}
+		}
+		else{
+			this.setState({
+				element: event.target,
+			});
+			event.target.classList.toggle("scaled");
+		}
 	}
 	render() {
 		return (
       <Fragment>
 			<div className='container'>
 				<h1>Hello Gustavo, Welcome to your Dashboard</h1>
-        <div className='box'>
-          <div className='item box__blue'/>
-          <div className='item box__red'/>
-          <div className='item box__green'/>
-          <div className='item box__purple'/>
+        <div className='box' id="box">
+          <div onClick={this.scaleButton} className='item box__blue'/>
+          <div onClick={this.scaleButton} className='item box__red'/>
+          <div onClick={this.scaleButton} className='item box__green'/>
+          <div onClick={this.scaleButton} className='item box__purple'/>
         </div>
 			</div>
       
